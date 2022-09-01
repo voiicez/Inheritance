adabf1894c740ddb787ef81a1c54ac78a7dd1081
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-09-01 14:04:36 +0300

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿Student std = new Student()
+{
+    Department = "Yazılım"
+};
+
+
+
+//inheritance-kalıtım,miras
+
+abstract class Person //base,parent,(abstract:soyut)
+{
+    public int Id {get;set;}
+    public string Name {get;set;}
+    public string Surname {get;set;}
+    
+    //string AdSoyadYazdir(); interface örneği
+    public string AdSoyadYazdir()
+    {
+        return Name + " " + Surname;
+    }
+}
+
+class Customer : Person
+{
+    public string KartNo {get;set;}
+    
+    //public string AdSoyadYazdir() Interface
+    //{
+    //    return Name + " " + Surname;
+    //}
+}
+class Student : Person
+{
+    public string Department {get;set;}
+    
+    //public string AdSoyadYazdir() Interface
+    //{
+    //    return Name + " " + Surname;
+    //}
+}
